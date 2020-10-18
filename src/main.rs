65597c703f0fe187c8b0f5f426c8b70d76e64c93
Cybercharge <ocharan@icloud.com>
2020-10-17 22:22:31 -0500

    Agregando un número aleatorio

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("¡Adivina el número!");
+
+    let numero_secreto = rand::thread_rng().gen_range(1, 101);
+
+    println!("El número secreto es: {}", numero_secreto);
+
     println!("Teclea tu intento.");
 
     let mut intento = String::new();
